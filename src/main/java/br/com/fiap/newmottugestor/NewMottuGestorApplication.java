531637b4a040e08635720dc073c56ca986f29911
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-22 20:32:59 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/newmottugestor/NewMottuGestorApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.newmottugestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewMottuGestorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewMottuGestorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/newmottugestor/NewMottuGestorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.newmottugestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewMottuGestorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
